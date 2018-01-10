f6c5657864542a04a7b9286465ff89b870020c18
Ataoglan <1306130074@ogr.iu.edu.tr>
2018-01-10 22:16:49 +0300

    Create Choise.cs

---
+++ Models/Choise.cs
@@ -0,0 +1,10 @@
+-using System;
+-
+-namespace api.Models{
+-    public class Choises{
+-      //  public int ChoiseID { get; set; }
+-        public int questionID { get; set; }
+-        public int Choise { get; set; }
+-        public string Text { get; set; }
+-        public bool CorrectAnswer { get; set; }
+-    }
